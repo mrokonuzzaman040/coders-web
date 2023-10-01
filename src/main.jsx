c4b5a93d79ad5e01bfd5562b4cc328b3337f236d
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-10-01 21:16:16 +0600

    added components and update route

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Home from './Components/Home/Home.jsx'
+import Login from './Components/LoginReg/Login/Login.jsx'
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Root from './Components/Root/Root.jsx';
+import Registration from './Components/LoginReg/Registration/Registration'
+import ErrorPage from './Components/ErrorPage/ErrorPage'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    errorElement: <div>Not found</div>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
-        element: <App></App>,
-      },
+        element: <Home></Home>,
+      }, {
+        path: "/login",
+        element: <Login></Login>,
+      }, {
+        path: "/register",
+        element: <Registration></Registration>,
+      }
     ],
   },
 ]);
