6c336ae1e98364ecaf686ca6b87d1628c038bfae
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-10-01 21:10:17 +0600

    adding components

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            Eroor Page
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <div>Not found</div>,
+    children: [
+      {
+        path: "/",
+        element: <App></App>,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
